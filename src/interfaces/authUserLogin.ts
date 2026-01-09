986b219bbb5bc3b8e1f23f61514d8b428afd3972
Reza Bahtiar <rezaosd339@gmail.com>
2026-01-09 20:48:03 +0800

    add file interface untuk user login

---
+++ src/interfaces/authUserLogin.ts
@@ -0,0 +1,37 @@
+import type { User, Role } from "../../generated/prisma/client"
+
+// interface untuk user login request
+export interface AuthUserLoginRequest {
+    email: string
+    password: string
+};
+
+// interface untuk user login response data
+export interface AuthUserLoginResponseData {
+    token?: string
+    id: number
+    name: string
+    email: string
+    role: Role
+};
+
+export interface AuthUserLoginResponse {
+    message: string
+    data: AuthUserLoginResponseData
+};
+
+// fungsi mapper untuk response user login
+export function toAuthUserLoginResponse(
+    user: User, token: string
+): AuthUserLoginResponse {
+    return {
+        message: "User logged in successfully",
+        data: {
+            token,
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+        }
+    }
+};
