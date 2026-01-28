6c7c8dcedbbf724ba54e5b6abd65f60e98c0685f
Reza Bahtiar <rezaosd339@gmail.com>
2026-01-28 20:08:33 +0800

    update route for otp login & register

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -2,6 +2,9 @@ import { Elysia } from "elysia";
 import { AuthController } from "../controllers/authController";
 import type { AuthUserRegisterRequest } from "../interfaces/authUserRegister";
 import type { AuthUserLoginRequest } from "../interfaces/authUserLogin";
+import { AuthOtpController } from "../controllers/authOtpController";
+import type { AuthRegisterOtpRequest, AuthVerifyRegisterOtpRequest } from "../interfaces/authOtpRegister";
+import type { AuthOtpLoginRequest, AuthOtpVerifyLoginRequest } from "../interfaces/authOtpLogin";
 import { LoginRateLimit } from "../middlewares/rateLimiter";
 
 // route untuk auth user
@@ -17,3 +20,30 @@ export const AuthRoute = new Elysia({ prefix: "/auth" })
         await LoginRateLimit(ctx);
         return AuthController.login(ctx.body as AuthUserLoginRequest);
     })
+
+
+// route untuk auth otp
+export const AuthOtpRoute = new Elysia({ prefix: "auth/otp" })
+    // req otp register
+    .post("/register", async ({ body, set }) => {
+        set.status = 200;
+        return AuthOtpController.requestOtpLogin(body as AuthRegisterOtpRequest);
+    })
+
+    // verify register otp
+    .post("/register/verify", async ({ body, set }) => {
+        set.status = 200;
+        return AuthOtpController.verifyRegisterOtp(body as AuthVerifyRegisterOtpRequest);
+    })
+
+    // req otp login
+    .post("/login", async ({ body, set }) => {
+        set.status = 200;
+        return AuthOtpController.requestOtpLogin(body as AuthOtpLoginRequest);
+    });
+
+    // verify otp login
+    AuthOtpRoute.post("/login/verify", async ({ body, set }) => {
+        set.status = 200;
+        return AuthOtpController.verifyOtpLogin(body as AuthOtpVerifyLoginRequest);
+    });
