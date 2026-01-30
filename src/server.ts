123154d83553c81a61eaeeaa58cc905776be3f06
Reza Bahtiar <rezaosd339@gmail.com>
2026-01-30 20:47:59 +0800

    add swagger doc

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,32 @@ import { AuthOtpRoute } from "./routes/authRoute";
 import { UserRoute } from "./routes/userRoute";
 import { ErrorMiddleware } from "./middlewares/errorMiddleware";
 import { startOtpCleanerJob } from "./helper/otpCleanerJob";
+import { swagger } from "@elysiajs/swagger";
 
 
 startOtpCleanerJob();
 
 const app = new Elysia()
+    .use(
+        swagger({
+            documentation: {
+                info: {
+                    title: "CRUD with Elysia, Bun and Prisma",
+                    version: "1.0.0",
+                    description: "Practice CRUD Backend API with JWT + OTP Login/Register"
+                },
+                components: {
+                    securitySchemes: {
+                        bearerAuth: {
+                            type: "http",
+                            scheme: "bearer",
+                            bearerFormat: "JWT"
+                        }
+                    }
+                }
+            }
+        })
+    )
     .use(cors({
         origin: true,
         credentials: true
