5129d838f344da6131685cf514a61782e79b9619
Reza Bahtiar <rezaosd339@gmail.com>
2026-01-30 21:08:40 +0800

    add schema swagger

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { AuthController } from "../controllers/authController";
 import type { AuthUserRegisterRequest } from "../interfaces/authUserRegister";
 import type { AuthUserLoginRequest } from "../interfaces/authUserLogin";
@@ -13,37 +13,79 @@ export const AuthRoute = new Elysia({ prefix: "/auth" })
     .post("/register", async ({ body, set }) => {
         set.status = 201;
         return AuthController.register(body as AuthUserRegisterRequest);
+    },{
+        tags: ["Auth Register new User"],
+        summary: "Register a new user",
+        body: t.Object({
+            name: t.String(),
+            email: t.String(),
+            password: t.String()
+        })
     })
     // login user
     .post("/login", async (ctx) => {
         ctx.set.status = 200;
         await LoginRateLimit(ctx);
         return AuthController.login(ctx.body as AuthUserLoginRequest);
+    }, {
+        tags: ["Auth Login User"],
+        summary: "Login user with email and password",
+        body: t.Object({
+            email: t.String(),
+            password: t.String()
+        })
     })
 
 
 // route untuk auth otp
-export const AuthOtpRoute = new Elysia({ prefix: "auth/otp" })
+export const AuthOtpRoute = new Elysia({ prefix: "/auth/otp" })
     // req otp register
     .post("/register", async ({ body, set }) => {
         set.status = 200;
         return AuthOtpController.requestOtpRegister(body as AuthRegisterOtpRequest);
+    }, {
+        tags: ["Auth OTP Register"],
+        summary: "Register a new user with otp",
+        body: t.Object({
+            email: t.String(),
+        })
     })
 
     // verify register otp
     .post("/register/verify", async ({ body, set }) => {
         set.status = 200;
         return AuthOtpController.verifyRegisterOtp(body as AuthVerifyRegisterOtpRequest);
+    }, {
+        tags: ["Auth verify OTP Register"],
+        summary: "Verify OTP for registering a new user",
+        body: t.Object({
+            email: t.String(),
+            code: t.String(),
+            name: t.String()
+        })
     })
 
     // req otp login
     .post("/login", async ({ body, set }) => {
         set.status = 200;
         return AuthOtpController.requestOtpLogin(body as AuthOtpLoginRequest);
+    }, {
+        tags: ["Auth OTP Login"],
+        summary: "Login user with otp",
+        body: t.Object({
+            email: t.String()
+        })
     })
 
     // verify otp login
     .post("/login/verify", async ({ body, set }) => {
         set.status = 200;
         return AuthOtpController.verifyOtpLogin(body as AuthOtpVerifyLoginRequest);
+    }, {
+        tags: ["Auth verify OTP Login"],
+        summary: "Verify OTP for login user",
+        body: t.Object({
+            email: t.String(),
+            code: t.String()
+        })
     })
