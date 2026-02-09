6e53690edc536b7931154db7b06906cdacedaa7d
Reza Bahtiar <rezaosd339@gmail.com>
2026-02-09 22:28:47 +0800

    fix fix

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,52 +1,15 @@
-import { Elysia } from "elysia";
 import jwt from "jsonwebtoken";
 import { ResponseError } from "../utils/responseError";
 import { logger } from "../utils/logging";
 import type { AuthContext } from "../@types/context";
 
-// export const AuthMiddleware = (app: Elysia) => app
-//     .derive(({ request }) => {
-//         // ambil token dari header
-//         const authHeader = request.headers.get("authorization");
-
-//         // cek jika token tidak ada
-//         if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//             logger.warn("Token not found");
-//             throw new ResponseError(
-//                 401,
-//                 "Unauthorized",
-//                 "Token not found"
-//             );
-//         }
-
-//         // parsing token
-//         const token = authHeader.split(" ")[1];
-
-//         // cek jika token tidak valid
-//         if (!token) {
-//             throw new ResponseError(
-//                 401,
-//                 "Unauthorized",
-//                 "Invalid token"
-//             );
-//         }
-
-//         const payload = jwt.verify(token, process.env.JWT_SECRET as string) as {
-//             id: number;
-//             role: string;
-//         };
-
-//         return {
-//             user: payload,
-//             token
-//         };
-//     })
-//     .as("global");
-
 export function AuthMiddleware(ctx: AuthContext) {
+  // ambil token dari header
   const authHeader = ctx.request.headers.get("authorization");
 
+  // cek jika token tidak ada
   if (!authHeader?.startsWith("Bearer ")) {
+    logger.warn("Token not found");
     throw new ResponseError(
       401,
       "TOKEN_NOT_FOUND",
@@ -54,8 +17,10 @@ export function AuthMiddleware(ctx: AuthContext) {
     );
   }
 
+  // parsing token
   const token = authHeader.split(" ")[1];
 
+  // cek jika token tidak valid
   if (!token) {
     throw new ResponseError(
       401,
