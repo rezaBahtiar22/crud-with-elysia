308c835063057b80fe899211c656947af211aa46
Reza Bahtiar <rezaosd339@gmail.com>
2026-02-13 22:09:07 +0800

    helper auth token

---
+++ src/utils/authToken.ts
@@ -0,0 +1,33 @@
+import crypto from "crypto";
+import { prisma } from "../database/prisma"
+import { generateToken } from "./jwt"
+import type { User } from "../../generated/prisma/client"
+
+export async function issueAuthTokens(user: User) {
+    const accessToken = generateToken({
+        id: user.id,
+        role: user.role
+    });
+
+    const refreshToken = crypto.randomBytes(32).toString("hex");
+
+    const hashedRefreshToken = crypto
+        .createHash("sha256")
+        .update(refreshToken)
+        .digest("hex");
+
+    await prisma.refreshToken.create({
+        data: {
+            userId: user.id,
+            tokens: hashedRefreshToken,
+            expiresAt: new Date(
+                Date.now() + 30 * 24 * 60 * 60 * 1000
+            )
+        }
+    });
+
+    return {
+        accessToken,
+        refreshToken
+    }
+}
