e3cc9abcaedaa06a78d9da511956814947b7d98f
Reza Bahtiar <rezaosd339@gmail.com>
2026-03-09 21:57:05 +0800

    -

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET ? process.env.JWT_SECRET : "NanaErzaaJFNC128736SHAU@#$%!hd&%d";
-const EXPIRED_IN = "3m";
+const EXPIRED_IN = "30m";
 
 // interface untuk payload JWT
 export interface JwtPayload {
